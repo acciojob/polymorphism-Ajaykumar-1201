88227630fcc2c30464ac1551de6db8ce12bac72d
Ajay Kumar <ajaymahto20319@gmail.com>
2023-05-15 00:53:25 +0530

    Donne

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -8,11 +8,16 @@ public class Main {
         public int product(int x, int y, int z) {
             return x * y * z;
         }
+
+        public double product(double x, double y) {
+            return x * y;
+        }
     }
     public static void main(String args[]) {
         product p = new product();
         int a = p.product(4, 5);
         int b = p.product(1, 2, 4);
+        double c = p.product(2.5, 7.6);
         //System.out.println(a);
     }
 
